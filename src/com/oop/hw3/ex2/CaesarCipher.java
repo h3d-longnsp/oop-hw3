eed34a223a916bff0de4ac1e911c72135a0e45b4
h3d-longnsp <hydroxyzine@metacity.email>
2023-10-09 22:06:01 +0700

    ex5: Caesar cipher
    Signed-off-by: h3d-longnsp <hydroxyzine@metacity.email>

---
+++ src/com/oop/hw3/ex2/CaesarCipher.java
@@ -0,0 +1,36 @@
+package com.oop.hw3.ex2;
+
+import java.util.Scanner;
+
+public class CaesarCipher {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a plain text string: ");
+        String plainText = scanner.nextLine().toUpperCase();
+        scanner.close();
+
+        String cipherText = encryptCaesarCipher(plainText);
+
+        System.out.println("The cipher text string is: " + cipherText);
+    }
+
+    public static String encryptCaesarCipher(String plainText) {
+        int shift = 3;
+        StringBuilder cipherText = new StringBuilder();
+
+        for (int i = 0; i < plainText.length(); i++) {
+            char character = plainText.charAt(i);
+
+            // Check if the character is a letter
+            if (Character.isLetter(character)) {
+                cipherText.append((char) ((character - 'A' + shift) % 26 + 'A'));
+            } else {
+                // If it's not a letter, leave it unchanged
+                cipherText.append(character);
+            }
+        }
+
+        return cipherText.toString().toUpperCase();
+    }
+}

552a6048d5805aed2aea83697ab3132cb9066c3f
h3d-longnsp <hydroxyzine@metacity.email>
2023-10-09 21:16:26 +0700

    ex1: factorial
    Signed-off-by: h3d-longnsp <hydroxyzine@metacity.email>

---
+++ src/com/oop/hw3/ex1/Factorial.java
@@ -0,0 +1,16 @@
+package com.oop.hw3.ex1;
+
+public class Factorial {
+    public static void main(String[] args) {
+        long input = 20;
+        System.out.printf("Factorial of %d is %d.\n", input, factorial(input));
+    }
+
+    public static long factorial(long number) {
+        if (number == 0 || number == 1) {
+            return 1;
+        } else {
+            return number * factorial(number - 1);
+        }
+    }
+}
